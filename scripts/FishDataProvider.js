02ae21f65e8603c6a1813a499031f9d6dd5e5065
LukeZvada <zvadamusic@gmail.com>
2020-07-13 15:27:28 -0500

    First Java

---
+++ scripts/FishDataProvider.js
@@ -0,0 +1,9 @@
+const fishCollection = [
+    {
+        name: "Nemo",
+        type: "Clown Fish",
+        Length: "5",
+        Location: "Great Barrier Reef",
+        Diet: "Plankton",
+    }
+]

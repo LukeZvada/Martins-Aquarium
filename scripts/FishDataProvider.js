a2673d3a57295021c7e2d6eb8b72f1995f7686b3
LukeZvada <zvadamusic@gmail.com>
2020-07-14 11:16:45 -0500

    FishDataProvider imported into Main.css

--- scripts/FishDataProvider.js
+++ scripts/FishDataProvider.js
@@ -1,9 +1,32 @@
 const fishCollection = [
+    { 
+        name: "Dory",
+        type: "blue Tang",
+        length: 4,
+        location: "Great Barrier Reef",
+        diet: "Plankton",
+        images: "./images/Blue Tang.jpg",
+    },
     {
         name: "Nemo",
         type: "Clown Fish",
-        Length: "5",
-        Location: "Great Barrier Reef",
-        Diet: "Plankton",
-    }
-]
+        length: 5,
+        location: "Great Barrier Reef",
+        diet: "Plankton",
+        images: "./images/Clown Fish.jpg",
+    },
+    {
+        name: "Ted",
+        type: "Marlin",
+        length: 14,
+        location: "Pacific and Atlantic Ocean",
+        diet: "Tuna",
+        images: "./images/Marlin.jpg",
+    },
+] 
+
+/* Export = Exporting this function out of this component and importing it into the main.css file*/
+export const useFish = () => { 
+    //.slice returns a copy of the fishCollection Array.
+    return fishCollection.slice() 
+}
---
+++ scripts/main.js
@@ -0,0 +1,12 @@
+//main.css is responsible for running the correct tasks when the site loads. 
+
+//importing the export function from FishDataProvider
+import { useFish } from "./FishDataProvider.js"
+
+const allTheFish = useFish()
+
+//Left hand side variable store indiviual items. Right hand (stores the entire array.
+//fish is the variable and can be named anything. Choose a name that provides context to teammates.
+for(const fish of allTheFish){
+    console.log(fish)
+}

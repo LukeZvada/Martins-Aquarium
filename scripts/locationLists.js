c3a7539e472e9204691c5e9be58f9201d8dda12f
LukeZvada <zvadamusic@gmail.com>
2020-07-22 15:18:46 -0500

    Main.js formatting 2

--- scripts/locationLists.js
+++ scripts/locationLists.js
@@ -1,21 +1,21 @@
-
 import {useLocation} from "./locationDataProvider.js"
 import {locationAsHTML} from "./locationHTMLMaker.js"
 
 export const locationList = () => {
     const contentElement = document.querySelector(".location")
-
     const locations = useLocation()
-
+    
     let locationHTMLRep = ""
     for(const location of locations) { 
         locationHTMLRep += locationAsHTML(location)
     }
-
+    
     contentElement.innerHTML += `
     <article>
         ${locationHTMLRep}
     </article>
     `
-    
 }
+
+
+    
